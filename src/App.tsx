461fbbcbfba059b40f66637bcb45ab19e859a231
bb21727 <bibin.baby@zs.com>
2021-12-10 00:05:54 +0530

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,16 @@ import inProgress from './assets/images/under-construction.gif';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={inProgress} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={inProgress} className="App-logo" alt="logo"/>
+                <p>
+                    We'll be up soon...
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
