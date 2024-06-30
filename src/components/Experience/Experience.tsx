b3684cc09b9b0a9a6acc11ed88886ae89e98cf7c
Naushad Sayyad <naushad.sayyad@affiliate.imd.org>
2024-06-30 22:07:45 +0530

    updates

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,49 +1,96 @@
 import { FC, useState } from "react";
-import { BodyContent, Container, Description, Section, Timeline, YearContent } from 'vertical-timeline-component-react';
+
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import TimelineOppositeContent, {
+  timelineOppositeContentClasses,
+} from "@mui/lab/TimelineOppositeContent";
+
+import "../Experience/Experience.scss";
 
 const customTheme = {
-    yearColor: '#405b73',
-    lineColor: '#d0cdc4',
-    dotColor: '#262626',
-    borderDotColor: '#d0cdc4',
-    titleColor: '#405b73',
-    subtitleColor: '#bf9765',
-    textColor: '#262626'
+  yearColor: "#405b73",
+  lineColor: "#d0cdc4",
+  dotColor: "#262626",
+  borderDotColor: "#d0cdc4",
+  titleColor: "#405b73",
+  subtitleColor: "#bf9765",
+  textColor: "#262626",
 };
 
 const monthDiff = (d1: any, d2: any) => {
-    let months;
-    months = (d2.getFullYear() - d1.getFullYear()) * 12;
-    months -= d1.getMonth();
-    months += d2.getMonth();
-    months = Math.abs(months)
-    months = months <= 0 ? 0 : months;
-    return `${months} Months`
-}
+  let months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  months = Math.abs(months);
+  months = months <= 0 ? 0 : months;
+  return `${months} Months`;
+};
 
 const Experience: FC<any> = (props: any) => {
-    const [timelineDetails] = useState(props.details.timeline)
-    let currentDate: any = new Date();
-    return timelineDetails && (
-        <div className='infoCard mt-3 px-4 py-3'>
-            <div className='cardTitle'>
-                Experience
-            </div>
-            <Timeline theme={customTheme} dateFormat='short'>
-                {timelineDetails.slice(0).reverse().map((timeline: any, index: number) => (
-                    <Container key={index}>
-                        <YearContent startDate={timeline.from} endDate={timeline.to ? timeline.to : currentDate}/>
-                        <BodyContent>
-                            <Section title={timeline.title}>
-                                <Description variant='subtitle' text={timeline.company + ', ' + timeline.location}/>
-                                <Description
-                                    text={timeline.timePeriod === 'Present' ? monthDiff(new Date(), new Date(timeline.from)) : timeline.timePeriod}/>
-                            </Section>
-                        </BodyContent>
-                    </Container>
-                ))}
-            </Timeline>
+  const [timelineDetails] = useState(props.details.timeline);
+  let currentDate: any = new Date();
+  return (
+    timelineDetails && (
+      <div className="infoCard mt-3 px-4 py-3">
+        <div className="cardTitle">Experience</div>
+
+        <div id="timeline">
+          <Timeline
+            sx={{
+              [`& .${timelineOppositeContentClasses.root}`]: {
+                flex: 0.2,
+              },
+            }}
+          >
+            {timelineDetails.reverse().map((item: any, index: number) => {
+              return (
+                <TimelineItem key={index}>
+                  <TimelineOppositeContent color="textSecondary">
+                    <div className="dateElement">
+                      <div className="subText">To </div>
+                      <div className="date">
+                        {item.to &&
+                          new Intl.DateTimeFormat("en-US", {
+                            dateStyle: "medium",
+                          }).format(new Date(item.to))}
+                        {!item.to && "Present"}
+                      </div>
+                      <div className="subText">From</div>
+                      <div className="date">
+                        {new Intl.DateTimeFormat("en-US", {
+                          dateStyle: "medium",
+                        }).format(new Date(item.from))}
+                      </div>
+                    </div>
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                    {index !== timelineDetails.length - 1 && (
+                      <TimelineConnector />
+                    )}
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <div className="Experience">
+                      <div className="Role">{item.title}</div>
+                      <div className="Company">
+                        {item.company},&nbsp;{item.location}
+                      </div>
+                      <div className="Duration">{item.timePeriod}</div>
+                    </div>
+                  </TimelineContent>
+                </TimelineItem>
+              );
+            })}
+          </Timeline>
         </div>
+      </div>
     )
-}
-export default Experience
+  );
+};
+export default Experience;
